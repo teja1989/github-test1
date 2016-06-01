d0226157ae1015c57fc11e695a6999d65fc5f815
teja1989 <tejatatini27@gmail.com>
2016-06-01 19:51:45 -0400

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,28 @@
+package helloWorld;
+
+public class helloworld {
+
+	public static void main(String[] args)
+	
+	{
+		// writing a sample program!!!
+		String Time;
+		int hh;
+		int mm; 
+		float ss;
+		hh = 01;
+		mm = 25;
+		ss = 23;
+		System.out.print("The Current Time is ");
+		System.out.print(hh);
+		System.out.print(":");
+		System.out.print(mm);
+		System.out.print(":");
+		System.out.println(ss);
+		System.out.print(ss/10);
+		
+		
+		
+	}
+
+}
